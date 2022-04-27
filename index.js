ef5d51e44e327ff1ba1b87aec5d0af2df0cf60fc
Moushumi Guha <moushumiguha@yahoo.com>
2022-04-26 20:29:31 -0400

    commit tree e0bbd449319cdad3dd932c2ce99d7b07fc7736dd

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Write your solution in this file!
+var customerName = 'bob';
+const leastFavoriteCustomer = 'doug'
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';     //if const is there then it would've lived in scope of this function. Now it is globally scoped.
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'bob'
+}
